854f4ddd203451736af9ffcab0165164f68ade0e
codeeon <yeon.develop@gmail.com>
2024-03-05 23:47:12 +0900

    Fix: validation

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import React, { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { Button } from '@/components/ui/button';
-import { z } from 'zod';
 import {
   Card,
   CardContent,
@@ -12,30 +8,36 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent } from '@/components/ui/tabs';
 
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { logInSchema } from '@/validators/auth';
+
 type LogInData = { email: string; password: string };
 
-const logInSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
-
-const LogInForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LogInData>({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
+const LogInForm: React.JSX.Element = () => {
   const [loginError, setLoginError] = useState('');
+
+  const form = useForm<LogInData>({
+    resolver: zodResolver(logInSchema),
+    defaultValues: { email: '', password: '' },
+  });
 
   const onSubmit: SubmitHandler<LogInData> = async (data) => {
     try {
@@ -58,36 +60,60 @@ const LogInForm = () => {
               <CardTitle>LOG IN</CardTitle>
               <CardDescription>Welcome to the Next.js world</CardDescription>
             </CardHeader>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <CardContent className='space-y-2'>
-                <div className='space-y-1'>
-                  <Label htmlFor='email'>Email</Label>
-                  <Input
-                    id='email'
-                    type='email'
-                    placeholder='abc@defgh.com'
-                    {...register('email', { required: true })}
-                  />
-                  {errors.email && <span className='text-red-500'>이메일을 입력해주세요.</span>}
-                </div>
-                <div className='space-y-1'>
-                  <Label htmlFor='password'>Password</Label>
-                  <Input
-                    id='password'
-                    type='password'
-                    placeholder='********'
-                    {...register('password', { required: true })}
-                  />
-                  {errors.password && (
-                    <span className='text-red-500'>비밀번호를 입력해주세요.</span>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <CardContent className='space-y-2'>
+                  <div className='space-y-1'>
+                    <FormField
+                      control={form.control}
+                      name='email'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input type={'email'} placeholder='abc@defgh.com' {...field} />
+                          </FormControl>
+                          {/* {errors.email && (
+                              <FormDescription>
+                                <span className='text-red-500'>이메일을 입력해주세요.</span>
+                              </FormDescription>
+                            )} */}
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className='space-y-1'>
+                    <FormField
+                      control={form.control}
+                      name='password'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input type={'password'} placeholder='********' {...field} />
+                          </FormControl>
+                          {/* {errors.password && (
+                              <FormDescription>
+                                <span className='text-red-500'>비밀번호를 입력해주세요.</span>
+                              </FormDescription>
+                            )} */}
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button type='submit'>Log In</Button>
+                  {loginError && (
+                    <FormDescription>
+                      <p className='text-red-500 ml-4'>{loginError}</p>
+                    </FormDescription>
                   )}
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button type='submit'>Log In</Button>
-                {loginError && <p className='text-red-500'>{loginError}</p>}
-              </CardFooter>
-            </form>
+                </CardFooter>
+              </form>
+            </Form>
           </Card>
         </TabsContent>
         <TabsContent value='password'></TabsContent>
