e634a20fa3b6c4ba9e5909757f19db0a6cf69c00
codeeon <yeon.develop@gmail.com>
2024-03-07 23:03:48 +0900

    Feat: root page Add a link that click to move to login page used next/link

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <Button>버튼</Button>
+    <div className='min-h-screen flex justify-center items-center'>
+      <Link href='/login'>
+        <h1 className='text-4xl'>Click here to login page</h1>
+      </Link>
     </div>
   );
 };
